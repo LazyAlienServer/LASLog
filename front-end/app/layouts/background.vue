6be9b00b2c0b9c7eb9656c193c48a5521a70b9ca
tanh_Heng <2196226582@qq.com>
2026-02-11 18:06:51 +0800

    feat: default home page

---
+++ front-end/app/layouts/background.vue
@@ -0,0 +1,21 @@
+<template>
+  <slot />
+  <div class="background">
+  </div>
+</template>
+<style>
+.background {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(rgba(223, 234, 250, 0.3), rgba(224, 209, 235, 0.3));
+    mask-image: url('/img/background-spot.svg');
+    mask-repeat: repeat;
+    mask-size: 67px 67px;
+    z-index: -100;
+    pointer-events: none;
+}
+</style>
---
+++ front-end/app/pages/index.vue
@@ -0,0 +1,176 @@
+<template>
+    <div class="content">
+        <div class="main-container">
+            <div class="left-container">
+                <h1>LAS&nbsp;LOG</h1>
+                <h2>管理您在LAS的一切事务</h2>
+                <div class="tag-container">
+                    <div class="tag" v-for="value in [['交流', '#A263CF'], ['分享', '#9774DA'], ['资料', '#8C84E5'], ['归档', '#8194F0']]" :style="{ backgroundColor: value[1] }">{{ value[0] }}</div>
+                </div>
+                <h3>Lazy Alien Server 的服务器交流平台，集成白名单管理、作品归档、插件管理、信息交流等功能</h3>
+            </div>
+            <div class="right-container">
+                <img src="/img/logo.svg" alt="LAS LOG Logo"/>
+            </div>
+        </div>
+        <div class="button-container">
+            <hr />
+            <a href="/user">探索 LAS LOG</a>
+            <hr />
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'background'
+})
+</script>
+<style lang="scss">
+@use "~/assets/main.scss";
+
+html {
+    background-color: #f8f8f8;
+    font-size: 18px;
+}
+
+h1 {
+    font-size: 8rem;
+    font-weight: 1000;
+    color: #746AEB;
+    letter-spacing: 0.25rem;
+    margin: 0px 0px -2rem 0px;
+}
+
+h2 {
+    font-size: 3.5rem;
+    letter-spacing: 0.25rem;
+    font-weight: 500;
+    color: #5C39A1;
+    margin: 0px;
+}
+
+h3 {
+    font-size: 1.1rem;
+    font-weight: 500;
+    color: #A263CF;
+    margin: 0px;
+    letter-spacing: 2px;
+    padding-bottom: 2rem;
+    padding-right: 15%;
+}
+
+img {
+    width: 34rem;
+    height: auto;
+}
+
+.content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 90vh;
+    width: 80%;
+    margin-left: 10%;
+}
+
+.main-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    width: 100%;
+}
+
+.tag-container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-flow: row;
+    gap: 1rem;
+    margin: 1rem 0px 2rem;
+}
+
+.tag {
+    font-size: 1.1rem;
+    font-weight: 600;
+    color: white;
+    letter-spacing: 0.2rem;
+    background-color: #5C39A1;
+    border-radius: 2.5rem;
+    padding: 0px 0.9rem 0px 1.1rem;
+}
+
+.button-container {
+    letter-spacing: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    gap: 2.5rem;
+    margin-top: 5%;
+    overflow: hidden;
+    hr {
+        width: 900px;
+        border: 0;
+        height: 2px;
+        background-color: #746AEB;
+        flex-shrink: 0;
+    }
+    a {
+        color: #746AEB;
+        font-size: 1.4rem;
+        font-weight: 400;
+        text-decoration: none;
+        white-space: nowrap;
+        flex-shrink: 0;
+        padding: 0.25rem 2.8rem;
+        border: #746AEB 2px solid;
+        border-radius: 0.7rem;
+        transition: all 0.2s ease-in-out;
+    }
+    a:hover {
+        background-color: rgba($color: #746AEB, $alpha: 0.1);
+    }
+}
+
+@media (max-width:1900px) {
+    html {
+        font-size: 16px;
+    };
+    
+}
+
+@media (max-width: 1600px) {
+    html {
+        font-size: 14px;
+    }
+}
+
+@media (max-width: 1400px) {
+    html {
+        font-size: 12px;
+    }
+    
+}
+
+@media (max-width: 1200px) {
+    html {
+        font-size: 16px;
+    }
+    .right-container {
+        display: none;
+    }
+    h3 {
+        padding-right: 0px;
+    }
+    .main-container {
+        justify-content: center;
+    }
+    .left-container {
+        text-align: center;
+    }
+    .tag-container {
+        justify-content: center;
+    }
+}
+</style>
