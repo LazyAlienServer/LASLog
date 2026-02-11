c6a663ad2313122a61282e9110d151a8372bbf87
tanh_Heng <2196226582@qq.com>
2026-02-11 21:12:19 +0800

    style: lint & style scope fix

--- front-end/app/layouts/background.vue
+++ front-end/app/layouts/background.vue
@@ -1,21 +1,23 @@
 <template>
-  <slot />
-  <div class="background">
+  <div>
+    <slot />
+    <div class="background" />
   </div>
 </template>
-<style>
+
+<style scoped lang="scss">
 .background {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-image: linear-gradient(rgba(223, 234, 250, 0.3), rgba(224, 209, 235, 0.3));
-    mask-image: url('/img/background-spot.svg');
-    mask-repeat: repeat;
-    mask-size: 67px 67px;
-    z-index: -100;
-    pointer-events: none;
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(rgba(223, 234, 250, 0.3), rgba(224, 209, 235, 0.3));
+  mask-image: url('/img/background-spot.svg');
+  mask-repeat: repeat;
+  mask-size: 67px 67px;
+  z-index: -100;
+  pointer-events: none;
 }
 </style>
