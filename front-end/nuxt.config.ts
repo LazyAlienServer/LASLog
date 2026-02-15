3b87cd188cb4ed92da588c054ec95600c20d92cd
Fanzhitianyu <fanzhitianyu@gmail.com>
2026-02-15 15:03:43 +0800

    feat(front-login):add login page

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -2,11 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  ssr: false,
+  debug: false,
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+  vite: {
+    plugins: [
+    ],
+  },
+  icon: {
+    customCollections: [
+      {
+        prefix: 'custom',
+        dir: './app/assets/icons/',
+        recursive: true,
+      },
+    ],
+  },
+  ui: {
+    fonts: false,
+  },
   eslint: {
     config: {
       standalone: false,
     },
   },
-  css: ['~/assets/css/main.scss'],
+  css: ['~/assets/css/main.scss', '~/assets/css/import.css'],
 })
