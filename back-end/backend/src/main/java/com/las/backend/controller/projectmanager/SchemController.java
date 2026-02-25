802f28f8b3b515714931b65d229b5866abfdde87
martin <sunyinuo62@gmail.com>
2026-02-25 16:13:02 +0800

    docs(projectManager): 添加swagger api文档

--- back-end/backend/src/main/java/com/las/backend/controller/projectmanager/SchemController.java
+++ back-end/backend/src/main/java/com/las/backend/controller/projectmanager/SchemController.java
@@ -3,6 +3,9 @@ package com.las.backend.controller.projectmanager;
 import com.las.backend.model.projectmanager.MaterialReq;
 import com.las.backend.service.projectmanager.SchemService;
 import com.las.backend.utils.result.Result;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,18 +15,25 @@ import java.util.concurrent.ExecutionException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/project-manager")
+@Tag(name = "项目管理", description = "项目管理接口")
 public class SchemController {
 
     private final SchemService schemService;
 
     @GetMapping("/getSchemFiles")
-    public Result getSchemFiles() throws ExecutionException, InterruptedException {return schemService.getSchemFiles();}
+    @Operation(summary = "获取原理图列表", description = "获取全部原理图列表")
+    public Result getSchemFiles() throws ExecutionException, InterruptedException {
+        return schemService.getSchemFiles();
+    }
 
     @GetMapping("/getProgress")
+    @Operation(summary = "获取进度", description = "获取全部方块数量与完成的方块数量以计算进度")
+    @Parameter(name = "id", description = "用户的唯一标识", required = true)
     public Result getProgress(@RequestParam String filename) {
         return schemService.getProgress(filename);
     }
 
-    @PostMapping("/getMissingMaterial")
+    @GetMapping("/getMissingMaterial")
+    @Operation(summary = "获取缺失材料", description = "获取缺失材料,包含材料区域")
     public Result getMissingMaterial(@RequestBody MaterialReq req) {return schemService.getMissingMaterial(req);}
 }
