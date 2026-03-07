207cd802b1bafb8da7697d34d2a184a8274aa0aa
Fanzhitianyu <Fanzhitianyu@gmail.com>
2026-03-07 21:30:10 +0800

    feat(TwoColumnLayout): 添加双列布局组件，支持自定义间距和响应式设计

---
+++ front-end/app/components/layouts/TwoColumnLayout.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  left?: string
+  right?: string
+  gap?: string
+  rowGap?: string
+}>(), {
+  left: '55vw',
+  right: 'auto',
+  gap: '40px',
+  rowGap: '2rem',
+})
+</script>
+
+<template>
+  <div class="two-column-layout" :style="{ 'gap': props.gap, '--rowGap': props.rowGap }">
+    <div class="left flex flex-col" :style="{ width: props.left, gap: props.rowGap }">
+      <slot name="left" />
+    </div>
+    <div class="right flex flex-col" :style="{ width: props.right, gap: props.rowGap }">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.two-column-layout {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 900px) {
+    flex-direction: column;
+    justify-content: start;
+    gap: var(--rowGap) !important;
+  }
+}
+
+.left,
+.right {
+  @media (max-width: 900px) {
+    width: auto !important;
+  }
+}
+</style>
